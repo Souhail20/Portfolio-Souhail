158a59da1749a27127183bbf8fa3935e22cbd890
Souhail khaled <bechirkhaled9@gmail.com>
2022-06-25 13:42:07 +0200

    first commit

---
+++ src/components/Projects/ProjectCards.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { BiLinkExternal } from "react-icons/bi";
+
+function ProjectCards({ title, imgPath, description, link, isBlog }) {
+  return (
+    <Card className="project-card-view">
+      <Card.Img variant="top" src={imgPath} alt="card-img" />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text style={{ textAlign: "justify" }}>
+          {description}
+        </Card.Text>
+        <Button variant="primary" href={link} target="_blank">
+          <BiLinkExternal /> &nbsp;
+          {isBlog ? "View Blog" : "View Project"}
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+export default ProjectCards;
